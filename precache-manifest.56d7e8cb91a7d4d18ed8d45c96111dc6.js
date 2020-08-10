7ab84ef04c3c2962e0e1c42f7a71f4ccffc57d7e
techSavvySaahil <saahil.madaan@gmail.com>
2020-08-11 02:41:20 +0530

    Updates

---
+++ precache-manifest.56d7e8cb91a7d4d18ed8d45c96111dc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f44a0254e78c63ff18a1317558b545b",
+    "url": "/youtube_search/index.html"
+  },
+  {
+    "revision": "a5992f3e51f3899ffa93",
+    "url": "/youtube_search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef0408f80a8127041821",
+    "url": "/youtube_search/static/js/2.3e71cff6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/youtube_search/static/js/2.3e71cff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5992f3e51f3899ffa93",
+    "url": "/youtube_search/static/js/main.caf063e3.chunk.js"
+  },
+  {
+    "revision": "0653d7a65cbba25baa94",
+    "url": "/youtube_search/static/js/runtime-main.af4040ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/youtube_search/static/media/logo.5d5d9eef.svg"
+  }
+]);
